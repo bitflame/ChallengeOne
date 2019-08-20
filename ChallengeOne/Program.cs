e88959a585e92ef936421e00f58126bde7e6d12a
Liquid Falme <sansari1@cox.net>
2019-08-20 15:40:27 -0400

    first commit

---
+++ ChallengeOne/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChallengeOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            names.Add("Shawn");
+            names.Add("Mary");
+            names.Add("Dave");
+            foreach  (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
